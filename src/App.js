fefb0f661c09626699d7cad392ce84c3ab6828d7
TommiVLavi <bgr912@myy.haaga-helia.fi>
2022-05-05 23:15:03 +0300

    Teht 20 suoritettu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 function App() {
-  return (
+  const GET_COUNTRIES = gql
+    `query GetCountries {
+      countries{code},
+      countries{name},
+      countries{continent{name}}
+    }`;
+
+  const { loading, error, data } = useQuery(GET_COUNTRIES);
+
+  if (loading) return <p className='App'>....LOADING....</p>;
+
+  else if (error) return <h1 className='App'>Oh! Something went wrong.</h1>
+
+  else return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tbody>
+            <tr><th>CODE</th><th>COUNTRY</th><th>CONTINENT</th></tr>
+            { data.countries.map((country, index) =>
+              {
+                return <tr key={index}>
+                  <td>{country.code}</td>
+                  <td>{country.name}</td>
+                  <td>{country.continent.name}</td>
+                </tr>;
+              }
+            )}
+          </tbody>
+        </table>  
       </header>
     </div>
   );
